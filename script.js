e5b760b45a630253c4e7edc0cc6eda123452f080
Nguyen Trung Kien <kien3710@gmail.com>
2022-02-21 17:08:39 +0700

    CV

---
+++ script.js
@@ -0,0 +1,90 @@
+//togge navbar------------------------------
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click" ,()=> {
+    hideSection();
+    toggeleNavbar();
+    document.body.classList.toggle("hide-scrolling");
+});
+function hideSection(){
+    document.querySelector("section.active").classList.toggle("fade-out");
+}
+function toggeleNavbar (){
+    document.querySelector(".header").classList.toggle("active");
+}
+//------------ACTIVE SETION------------------------------------------
+document.addEventListener("click", (e)=>{
+    if(e.target.classList.contains("link-item") && e.target.hash !== ""){
+        //active overlay
+        document.querySelector(".overlay").classList.add("active");
+        navToggler.classList.add("hide");
+        if(e.target.classList.contains("nav-item")){
+            toggeleNavbar();
+        }
+        else { 
+            hideSection();
+            document.body.classList.add("hide-scrolling");
+        }
+        setTimeout(() => {
+            document.querySelector("section.active").classList.remove("active","fade-out");
+            document.querySelector(e.target.hash).classList.add("active");
+            window.scrollTo(0,0); 
+            document.body.classList.remove("hide-scrolling");
+            navToggler.classList.remove("hide");
+            document.querySelector(".overlay").classList.remove("active");
+
+        }, 500);
+    } 
+});
+
+
+
+
+// ---------------------about tabs----------------
+const tabsContainer = document.querySelector(".about-tab"),
+aboutSection = document.querySelector(".about-section");
+
+tabsContainer.addEventListener("click", (e) =>{
+    if(e.target.classList.contains("tab-item") && !e.target.classList.contains("active")){
+        tabsContainer.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
+        const target = e.target.getAttribute("data-target");
+        aboutSection.querySelector(".tab-content.active").classList.remove("active");
+        aboutSection.querySelector(target).classList.add("active"); 
+    };
+})
+//-------------------------portfolio item dentail------------------------
+document.addEventListener("click", (e)=>{
+    if(e.target.classList.contains("view-project-btn")){
+        toggelePortfolioPopup();
+        document.querySelector(".prortfolio-popup").scrollTo(0,0);
+        portfolioItemDetails(e.target.parentElement);
+
+    }
+})
+function toggelePortfolioPopup(){
+    document.querySelector(".prortfolio-popup").classList.toggle("open");
+    document.body.classList.toggle("hide-scrolling");
+    document.querySelector(".main ").classList.toggle ("fade-out");
+}
+document.querySelector(".pp-close").addEventListener("click", toggelePortfolioPopup);
+//hidden popu when click ourside   
+document.addEventListener("click", (e) =>{
+    if(e.target.classList.contains("pp-inner")){
+        toggelePortfolioPopup();
+    }
+}) 
+
+function portfolioItemDetails(porfolioItem){
+    document.querySelector(".pp-thumbnail img ").src = 
+    porfolioItem.querySelector(".protfolio-item-thumbnail img").src;
+
+    document.querySelector(".pp-header h3").innerHTML = 
+    porfolioItem.querySelector(".protfolio-item-title").innerHTML;
+
+    document.querySelector(".pp-body").innerHTML =
+    porfolioItem.querySelector(".portfolio-item-details").innerHTML;
+
+
+}
+
+
